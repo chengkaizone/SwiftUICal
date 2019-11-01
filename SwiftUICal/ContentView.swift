1f4b0b99e7eb6e87735955f1705c05849b686a5d
tony <ganchengkai@joinhov.com>
2019-11-01 14:45:21 +0800

    Initial Commit

---
+++ SwiftUICal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICal
+//
+//  Created by tony on 2019/11/1.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
